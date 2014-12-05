e1567d2369307d7587bbed1a110e7736bce9d921
xinzhi.zhang <xinzhi.zhang@alipay.com>
2014-12-05 14:40:15 +0800

    modify

--- src/main/java/allen/memoryutil/other/TestForMap.java
+++ src/main/java/allen/memoryutil/other/TestForMap.java
@@ -13,13 +13,13 @@ public class TestForMap {
     }
 
     private static void allenTest() {
-        TestMemory.printObjectInShort("12, 10000",
+        TestMemory.printObjectInShort("1200, 10000",
                 putKeyAndValue(new HashMap<Object, Object>(), 12, 10000));
-        TestMemory.printObjectInShort("12, 20000",
+        TestMemory.printObjectInShort("1200, 20000",
                 putKeyAndValue(new HashMap<Object, Object>(), 12, 20000));
-        TestMemory.printObjectInShort("16, 10000",
+        TestMemory.printObjectInShort("1600, 10000",
                 putKeyAndValue(new HashMap<Object, Object>(), 16, 10000));
-        TestMemory.printObjectInShort("16, 20000",
+        TestMemory.printObjectInShort("1600, 20000",
                 putKeyAndValue(new HashMap<Object, Object>(), 16, 20000));
 
     }
